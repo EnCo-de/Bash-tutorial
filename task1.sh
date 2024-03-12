bf9d684ccc514efadffcd458ea9e6272410846cb
EnCo-de <142403662+EnCo-de@users.noreply.github.com>
2024-03-12 16:50:06 +0400

    Update task1.sh
    capitalize first letters of each word

--- task1.sh
+++ task1.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-filename=$1;
+filename=$1
+domain=@abc.com
 echo "CSV file name is '$filename'."
-cat $filename
+# cat $filename
+echo ""
 
-while read line
+echo > accounts_new.csv
+while IFS="," read -r user_id location_id full_name email
 do
-   echo "Record is : $line"
+   IFS=' '
+   read -r name surname <<< "${full_name,,}"
+   echo "Record is : ${name^} ${surname^}" >> accounts_new.csv
 done < $filename
+echo ""
+printf "\n"
+cat accounts_new.csv
+
+# Note that we’re setting the Input Field Separator (IFS) to “,”  in the while loop. As a result, we can parse the comma-delimited field values into Bash variables using the read command.
+# while IFS="," read -r rec_column1 rec_column2 rec_column3 rec_column4
+# do
+#   echo "Displaying Record-$rec_column1"
+#   echo "Quantity: $rec_column2"
+#   echo "Price: $rec_column3"
+#   echo "Value: $rec_column4"
+#   echo ""
+# done < <(tail -n +2 input.csv)
