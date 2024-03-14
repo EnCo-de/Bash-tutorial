977ac8f96cac764d63a016e8fa8531f266b83d9d
EnCo-de <142403662+EnCo-de@users.noreply.github.com>
2024-03-14 22:00:36 +0400

    Update task2.sh

--- task2.sh
+++ task2.sh
@@ -9,33 +9,25 @@ map["testName"]=$(awk -F'\\[ | \\]' '{print $2}' <<< "$header")
 printf "%s\n" "${!map[@]}" "${map[@]}" | pr -2t
 
 footer=$(tail -n 1 $filename)
-nums=($(grep -Eo '[0-9]+' <<< "$footer"))
+nums=($(grep -Eo '[0-9.]+' <<< "$footer"))
 declare -A summary=( ['success']=${nums[0]} ['total']=${nums[1]} ['failed']=${nums[2]} ['rating']=${nums[3]} ['duration']="${nums[4]}ms" )
 printf "%s\n" "${!summary[@]}" "${summary[@]}" | pr -2t
 
-
-
-tests=()
 while read -r line
 do
-   echo $line
+   echo ; echo $line
+
    first_word="${line%% *}"
-   if [ "$first_word" = "not" ]; then
-        # echo "Strings are equal."
-        status=false
-      echo "[$status]=false"
-    elif [ "$first_word" = "ok" ]; then
+   if [ "$first_word" = "ok" ]; then
         status=true
-        # echo "Strings are not equal."
-      echo "[$status]=true"
+    elif [ "$first_word" = "not" ]; then
+        status=false
     fi
 
    last_word=$(sed 's/.*[[:blank:]]//' <<< "$line") # | cat -e
-   echo "last=$last_word"
-
-    name=$(awk -F'[0-9]+  |, [0-9]+' '{print $2}' <<< "$line")
-    # name=$(awk '{ sub(/.*BEGIN:/, ""); sub(/END:.*/, ""); print }')
-   sus=( 222 333.3)
+   name=$(awk -F'[0-9]+  |, [0-9]+' '{print $2}' <<< "$line")
    declare -A test=( ['name']="$name" ['status']="$status" ['duration']="$last_word")
-   printf "%s\n" "${!test[@]}" "${test[@]}" 
-done < <(head -n -2 $filename | tail -n -2)
+   printf "%s\n" "${!test[@]}" "${test[@]}" | pr -2t
+done < <( tail -n +3 $filename | head -n -2 )
+
+# jo -p name=JP object=$(jo fruit=Orange hungry@0 point=$(jo x=10 y=20 list=$(jo -a 1 2 3 4 5)) number=17) sunday@0
